bfe9ecfa1d9f41e8bcc280fb333caea8d0b6e26c
bravoosonja <bravoosonja@gmail.com>
2022-05-04 07:47:38 +1000

    update UI

--- src/ui.js
+++ src/ui.js
@@ -16,17 +16,7 @@ export default class UI {
     return element;
   }
 
-  static render() {
-    // const header = UI.createHtmlElement('div', 'header', null, 'TO-DO LIST');
-    // document.body.appendChild(header);
+  static render(filters) {
 
-    // const content = UI.createHtmlElement('div', 'content', null, null);
-    // document.body.appendChild(content);
-
-    // const tasksGrid = UI.createHtmlElement('div', 'tasks-grid', ['tasks'], null);
-    // content.appendChild(tasksGrid);
-
-    // const projectsContainer = UI.createHtmlElement('div', 'projects-container', null);
-    // content.appendChild(projectsContainer);
   }
 }
